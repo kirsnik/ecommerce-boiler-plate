e19bda5fb5f41b2c422b4b64be20fcc67d61bfa0
kirsnik <gwingster@gmail.com>
2019-06-11 23:20:30 -0700

    added redux code. Modyfing current code to allow mysql vs mongo

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -10,7 +9,7 @@ var PORT = process.env.PORT || 3000;
 // code to be added
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("build"));
+app.use(express.static("dist"));
 app.use(express.static('public'));
 app.use(express.static('uploads'));
 
@@ -24,15 +23,11 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 
-// Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
+app.listen(PORT, function () {
+  console.log(
+    "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
+    PORT,
+    PORT
+  );
 });
-
 module.exports = app;
